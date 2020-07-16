1222f3620164147cc923ff6ed48bd68c2a59b91f
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-16 07:20:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,4 +2,13 @@ sort_array_asc = [3, 5, 1, 65, 23, 104, 3, 54]
 
 sort_array_asc.sort do |a, b|
   a <=> b 
-endf
+end
+
+
+def swap_elements_from_to(array, index, destination_index)
+    item = array[index]
+    array.delete_at(index)
+    array.insert(destination_index, item)
+    array
+
+end
